2eb1ca1e7573edb15bac9fadcc89ffbed9811943
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-05-05 22:19:50 +0000

    python_list

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-	str = ""
-	for x in my_string:
-		if x is not 'c' and x is not 'C':
-            		str = str + x
-	return(str)
+    str = ""
+    for x in my_string:
+        if x is not 'c' and x is not 'C':
+            str = str + x
+    return(str)
