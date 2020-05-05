ec18e5dd07293c595211dcd137dde663f2c51de2
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-05-05 22:06:42 +0000

    p

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-	r = len(my_list)
-	if idx < 0 or idx > r:
-		return (None)
-	for x in range(r):
-		if x == idx:
-			return(my_list[x])
+    r = len(my_list)
+    if idx < 0 or idx > r:
+        return (None)
+    for x in range(r):
+        if x == idx:
+            return(my_list[x])
