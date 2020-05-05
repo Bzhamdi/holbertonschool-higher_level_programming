f28c03e8d348efceb2136464912f94bca8cb383c
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-05-05 22:41:28 +0000

    puyhon_list

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,6 +2,21 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a = len(tuple_a)
     b = len(tuple_b)
-    new = ((tuple_a[0] if a >= 1 else 0) + (tuple_b[0] if b >= 1 else 0),
-        (tuple_a[1] if a >= 2 else 0) + (tuple_b[1] if b >= 2 else 0))
+    if a >= 1:
+        a1 = tuple_a[0]
+    else:
+        a1 = 0
+    if b >= 1:
+        b1 = tuple_b[0]
+    else:
+        b1 = 0
+    if a >= 2:
+        a2 = tuple_a[1]
+    else:
+        a2 = 0
+    if b >= 2:
+        b2 = tuple_b[1]
+    else:
+        b2 = 0
+    new = (a1 + b1, a2 + b2)
     return (new)
