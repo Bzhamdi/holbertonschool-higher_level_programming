4e284b1eaba2da6e3cc8524657cc97b673cc4c07
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-05-25 07:24:29 +0000

    rectangel_area_primeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Class Rectangle
+"""
+
+
+class Rectangle:
+    """Rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initializes the rectangle"""
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        """height.getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height.setter"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """width.getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width.setter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        """ rectangle.area """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """ rectangle.perimeter """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__height + self.__width) * 2
