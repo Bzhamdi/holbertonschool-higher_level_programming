5f905982256ee35aaf8b5a93bfaceddd6673c0ea
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-06-03 17:08:27 +0000

    Student to disk and reload

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Student"""
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        """constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Student instance to jason with filter
+        with specified attributes"""
+        if attrs is None:
+            return self.__dict__
+        dict = {}
+        for i in attrs:
+            if i in self.__dict__:
+                dict[i] = self.__dict__[i]
+        return dict
+
+    def reload_from_json(self, json):
+        """replaces attributes of the Student instance"""
+        for i in json:
+            try:
+                setattr(self, i, json[i])
+            except:
+                pass
