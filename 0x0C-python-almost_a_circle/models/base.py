0e30e7ac72b8b2b2b78aa59249b75a468eb4499b
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-06-09 17:05:01 +0000

    jason

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@ module contains the Base
 """
 
 
+import json
+
+
 class Base:
     " the Base class"
     __nb_objects = 0
@@ -15,3 +18,23 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of a list of dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        file_name = cls.__name__ + ".json"
+        dic = []
+        if list_objs is not None or len(list_objs) <= 0:
+            for i in list_objs:
+                dic.append(cls.to_dictionary(i))
+        with open(file_name, 'w') as file:
+            file.write(cls.to_json_string(dic))
