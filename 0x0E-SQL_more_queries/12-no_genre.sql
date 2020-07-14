15d5e58117dccaca8136620e122ca7c2c8a57185
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-07-14 22:37:20 +0100

    o

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,3 +1,3 @@
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked. 
 
-Select t.title , tg.genre_id FROM tv_show_genres tg LEFT JOIN tv_shows t ON t.id = tg.show_id WHERE tg.show_id IS NULL ORDER BY t.title, tg.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id WHERE tv_show_genres.show_id IS NULL ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
