b09f51479413fd6885c152b729f83841b2fa9d02
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-07-14 22:19:42 +0100

    o

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,3 +1,3 @@
 -- selct 
 
-SELECT t.title, tg.genre_id FROM tv_shows t LEFT JOIN tv_shows_genres tg ON tg.show_id=t.id ORDER BY t.title, tg.genre_id;
+SELECT t.title, tg.genre_id FROM tv_shows t LEFT JOIN tv_show_genres tg ON tg.show_id=t.id ORDER BY t.title, tg.genre_id;
