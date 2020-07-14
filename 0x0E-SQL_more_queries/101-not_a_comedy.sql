494b0c768e41724add28e5dfe29da591149fb17f
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-07-14 23:51:00 +0100

    h

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,5 +1,4 @@
 -- lists all shows without the genre Comedy in the database 
 
-SELECT ts.title FROM tv_shows ts WHERE ts.id NOT IN (SELECT ts.id FROM tv_shows ts INNER JOIN tv_show_genres tsg ON ts.id = tsg.show_id INNER JOIN tv_genres tg ON tsg.genre_id = ts.id
-WHERE tg.name = "Comedy")
-ORDER BY ts.title;
+SELECT DISTINCT ts.title 
+FROM tv_shows ts WHERE ts.id NOT IN (SELECT show_id FROM tv_show_genres tsg INNER JOIN tv_genres tg ON tg.id = stg.genre_id WHERE tg.name = 'Comedy') ORDER BY ts.title ASC; 
