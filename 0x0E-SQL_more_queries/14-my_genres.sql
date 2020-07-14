6bbedbd48d86123988c4a0efb13c1d37b53f97b0
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-07-14 23:06:47 +0100

    join

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,3 @@
 -- lists all genres of the show
-SELECT t.name FROM tv_genres t
-JOIN tv_show_genres tg ON t.id = tg.genre_id
-JOIN tv_shows ts ON ts.id = tg.show_id
-WHERE ts.title = 'Dexter' ORDER BY tg.name;
+
+SELECT tg.name FROM  tv_shows ts JOIN tv_show_genres tsg ON ts.id = tsg.show_id JOIN tv_genres tg ON tg.id = tsg.genre_id WHERE ts.title = 'Dexter' ORDER BY tg.name;
