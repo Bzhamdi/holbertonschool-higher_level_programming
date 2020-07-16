e0bd36077a7394ac820cdea35c552ff317fc5c3a
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-07-16 18:12:29 +0000

    join

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,4 +1,5 @@
 -- lists all shows from hbtn_0d_tvshows_rate by their rating.
 
-SELECT t.title, SUM(tsg.rate) AS rating FROM tv_shows ts INNER JOIN tv_show_ratings tsg
-ON ts.id = tsg.show_id GROUP BY ts.id ORDER BY rating DESC;
+SELECT ts.title, SUM(tsr.rate) AS rating
+FROM tv_shows  ts JOIN tv_show_ratings tsr ON tsr.show_id =  ts.id
+GROUP BY ts.title ORDER BY rating DESC;
