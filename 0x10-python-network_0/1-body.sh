00e68fd41ba50ddfeec271ad73301274af2e8153
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-08-18 12:32:27 -0400

    displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL, and displays the body of the response
-# -S option to disable the progression display
-# -L option to follow all redirects
 curl -s -L "$1"
