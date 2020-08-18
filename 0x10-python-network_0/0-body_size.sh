22b6b03f68b4dfb8908375ff7e1e58dbd0a3a534
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-08-18 11:11:34 -0400

    body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #result Content-Length: 10 so 
 #awk -F : field separator. OR  cut -d
-curl -sI $1 | grep 'Content-Length:' | awk -F " " '{print $2}'
+curl -sI "$1" | grep 'Content-Length:' | awk -F " " '{print $2}'
