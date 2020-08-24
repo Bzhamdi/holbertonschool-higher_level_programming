61d152fafe7097f802d0fb9f8d417c2c0495a108
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-08-24 19:55:18 -0400

    api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+post and a response as json 
+"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    
+    if len(sys.argv) == 2:
+        q = sys.argv[1]
+    else:
+        q = ""
+    src = {'q': q}  
+    response = requests.post('http://0.0.0.0:5000/search_user', data=src)
+    try:
+        res_json = response.json()
+        id = res_json.get('id')
+        name = res_json.get('name')
+        if res_json == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(res_json.get('id'), res_json.get('name')))
+    except:
+        print("Not a valid JSON")
