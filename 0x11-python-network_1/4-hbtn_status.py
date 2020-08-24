36f50d4d6280615ec8a263f7eca43b3aa75579cf
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-08-24 17:24:37 -0400

    crud get

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,11 +2,12 @@
 """Paython script that fetches https://intranet.hbtn.io/status
 with urllib
 """
-import urllib.request
+import requests
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read().decode()
+
+    html = requests.get('https://intranet.hbtn.io/status')
+    html = html.text
     print("Body response:")
     print("\t- type: {}".format(type(html)))
     print("\t- content: {}".format(html))
