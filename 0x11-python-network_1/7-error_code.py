31fe01737899fbd4e0c17c7d9ff2b1ca5c3a0fb0
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-08-24 19:31:12 -0400

    error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,14 +1,15 @@
+#!/usr/bin/python3
 import requests
 import sys
 """
 error code
 """
 
-if __name__ == "__main__":
-
+try:
     response = requests.get(sys.argv[1])
 
-    if response.status_code <= 400:
-        print(response.text)
-    else:
-        print("Error code: {}".format(response.status_code))
+        
+    print(response.text)
+    
+except Exception as err:
+    print("Error code: {}".format(response.status_code))
