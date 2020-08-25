cfbde9412c485b56bd935627488d36023888abca
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-08-24 23:47:40 -0400

    crud get from githu

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,11 +6,12 @@ crud get response get top 10 commit from github
 import requests
 import sys
 
-r = requests.get('https://api.github.com/repos/{}/{}/commits'
-                 .format(sys.argv[2], sys.argv[1]))
-repos = r.json()
-for x in repos[0:10]:
-    print(
-        "{}: {}".format(
-            x.get('sha'),
-            x.get('commit').get('author').get('name')))
+if __name__ == '__main__':
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(sys.argv[2], sys.argv[1]))
+    repos = r.json()
+    for x in repos[0:10]:
+        print(
+            "{}: {}".format(
+                x.get('sha'),
+                x.get('commit').get('author').get('name')))
