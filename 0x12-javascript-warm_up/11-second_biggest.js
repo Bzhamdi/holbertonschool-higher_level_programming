6d619de3c7ec61080613c4fba7d31b084cd82563
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-09-16 13:22:31 -0400

    big

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,17 @@
 #!/usr/bin/node
 /* js is a number */
 
+if (process.argv.length === 4) {
+  if ((parseInt(process.argv[2]) > parseInt(process.argv[3]))) {
+    console.log(process.argv[3]);
+  } else {
+    console.log(process.argv[2]);
+  }
+}
+
 if (process.argv.length < 4) {
   console.log(0);
-} else {
+} else if (process.argv.length > 4) {
   let max = process.argv[2];
   let max2 = process.argv[2];
   for (let i = 3; i <= process.argv.length; i++) {
@@ -12,8 +20,8 @@ if (process.argv.length < 4) {
     }
   }
   for (let j = 3; j <= process.argv.length; j++) {
-    if (parseInt(process.argv[j]) > parseInt(max2) && parseInt(process.argv[j]) < parseInt(max)) {
-      max2 = process.argv[j];
+    if (process.argv[j] > max2 && process.argv[j] < max) {
+      max2 = parseInt(process.argv[j]);
     }
   }
   console.log(max2);
