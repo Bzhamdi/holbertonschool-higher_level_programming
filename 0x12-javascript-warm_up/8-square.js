043ae9d598c938135e1e5ee6d3f498b1c7ce8d3b
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-09-15 23:44:28 -0400

    loop and repeat

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* js is a number */
+if (process.argv.length < 2 || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('X'.repeat(process.argv[2]));
+  }
+}
