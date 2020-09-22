26f86a3ab65e4fcc5c60e18e7e9ae07fab1b100c
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-09-21 21:09:35 -0400

    rev

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,8 +2,8 @@
 exports.esrever = function (list) {
   /* reverse list with js */
   const rev = [];
-  let i = list.length;
-  for (i - 1; i >= 0; i--) {
+
+  for (let i = list.length - 1; i >= 0; --i) {
     rev.push(list[i]);
   }
   return rev;
