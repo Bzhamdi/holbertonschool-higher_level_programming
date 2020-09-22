5f6b2639324fd364801bc5dc1d61f672c3fe1608
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-09-22 01:37:54 -0400

    concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
 var fs = require('fs');
-var uglify = require("uglify-js");
-var uglified = uglify.minify([process.argv[2], process.argv[3]]);
-fs.writeFile(process.argv[4], uglified.code);
+fs.writeFile(process.argv[4], fs.readFileSync(process.argv[2])+fs.readFileSync(process.argv[3]));
