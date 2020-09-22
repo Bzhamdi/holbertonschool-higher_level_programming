ef20b356bdb761e01702c49abbe640c0ccdf985a
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-09-22 17:42:59 -0400

    script that reads and prints the content of a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (err, text) {
-  if (err || text) {
-    console.log(err || text);
-  }
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  console.log(err || data);
 });
