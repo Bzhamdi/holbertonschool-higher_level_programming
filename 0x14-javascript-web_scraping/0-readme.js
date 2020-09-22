2cc76df259578923ffea4a4362f9179676fd50f5
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-09-22 17:46:22 -0400

    reads and prints the content of a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  console.log(err || data);
+  if (err || data) {
+    console.log(err || data);
+  }
 });
