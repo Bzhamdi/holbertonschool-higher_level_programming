a9c39c9e7f150cb9a1ef1623aea057967ad34fdb
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-09-22 20:31:03 -0400

    get request into get request

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*  script that prints all characters of a Star Wars movie */
+const fs = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+fs(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+  const data = JSON.parse(body);
+  for (const movie of data.characters) {
+    fs(movie, function (errr, responsee, bodyy) {
+      console.log(JSON.parse(bodyy).name);
+    });
+  }
+});
